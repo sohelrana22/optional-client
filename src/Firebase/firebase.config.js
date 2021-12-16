ffeae5e34c8352673c16a0742feb674c9ea5fd78
sohelrana22 <sohel252646@gmail.com>
2021-12-16 23:15:43 +0600

    firebase added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrQegG9iQzE9xdUo8ajIFDsMm71P0LrDM",
+    authDomain: "optional-2b8f3.firebaseapp.com",
+    projectId: "optional-2b8f3",
+    storageBucket: "optional-2b8f3.appspot.com",
+    messagingSenderId: "537719668825",
+    appId: "1:537719668825:web:80a3eeb21089c61e467148"
+  };
+  export default firebaseConfig;
